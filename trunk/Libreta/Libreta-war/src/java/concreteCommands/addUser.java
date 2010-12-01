156dda707c13572616a93c9a9bbfebae427864f0
leitesgustavo <leitesgustavo@c292032a-868c-c56d-4c54-da6379c2254a>
2010-12-01 23:19:26 +0000

    Update addUser

--- trunk/Libreta/Libreta-war/src/java/concreteCommands/addUser.java
+++ trunk/Libreta/Libreta-war/src/java/concreteCommands/addUser.java
@@ -8,11 +8,15 @@ package concreteCommands;
 
 import ejb.ABMUsuarioLocal;
 import java.io.IOException;
+import java.text.ParseException;
 import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import frameworkp.Command;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.naming.Context;
@@ -26,6 +30,10 @@ public class addUser implements Command {
 
     private String Nombre;
     private String Apellido;
+    private String login;
+    private String pwd;
+    private Date fNac;
+    private String rol;
 
     public addUser() {
     }
@@ -46,9 +54,37 @@ public class addUser implements Command {
         this.Nombre = Nombre;
     }
 
+    public Date getfNac() {
+        return fNac;
+    }
 
+    public void setfNac(Date fNac) {
+        this.fNac = fNac;
+    }
 
+    public String getLogin() {
+        return login;
+    }
 
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
 
   @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -56,21 +92,27 @@ public class addUser implements Command {
       try {
             this.Nombre = request.getParameter("nombre");
             this.Apellido = request.getParameter("apellido");
+            this.login = request.getParameter("usuario");
+            this.pwd = request.getParameter("pwd");
+
+            DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+
+            this.fNac = df.parse(request.getParameter("fNac"));
+            this.rol = request.getParameter("rol");
             Context ctx = new InitialContext();
             ABMUsuarioLocal ejbUsuario = (ABMUsuarioLocal) ctx.lookup("java:comp/env/ABMUsuario");
             //request.setAttribute("usuario", this.login);
-            return ejbUsuario.alta(Nombre, Nombre, Nombre, Apellido);
+            
+            return ejbUsuario.alta(this.login,this.pwd,this.Nombre,this.Apellido,this.fNac,this.rol);
 
 
+        } catch (ParseException ex) {
+            Logger.getLogger(addUser.class.getName()).log(Level.SEVERE, null, ex);
         } catch (NamingException ex) {
             Logger.getLogger(ComandoConcreto.class.getName()).log(Level.SEVERE, "No se pudo encontrar el EJB", ex);
         }
 
         return "";
-
-
-
-
 
     }
 
