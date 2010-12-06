0744801064e055bdff6a707e5efd5186edb6a0ce
pablodmv <pablodmv@c292032a-868c-c56d-4c54-da6379c2254a>
2010-12-06 22:40:56 +0000

    persistencia completa de usuario y contacto con consultas

--- trunk/Libreta/Libreta-ejb/src/java/ejb/ABMUsuario.java
+++ trunk/Libreta/Libreta-ejb/src/java/ejb/ABMUsuario.java
@@ -6,16 +6,18 @@
 package ejb;
 
 import entities.Usuario;
-import entities.Contacto;
-import entities.Direccion;
 import entities.Credenciales;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import util.TipoDireccion;
 
 /**
  *
@@ -53,6 +55,28 @@ public class ABMUsuario implements ABMUsuarioLocal {
 
     }
 
+//    public String getEncoded(String texto, String algoritmo){
+//         String output="";
+//
+//        try {
+//
+//            byte[] textBytes;
+//           textBytes = texto.getBytes("UTF-8");
+//
+//            MessageDigest md = MessageDigest.getInstance(algoritmo);
+//            md.update(textBytes);
+//            byte[] codigo = md.digest();
+//            output = new String(codigo);
+//
+//        } catch (UnsupportedEncodingException ex) {
+//                Logger.getLogger(ABMUsuario.class.getName()).log(Level.SEVERE, null, ex);
+//
+//        } catch (NoSuchAlgorithmException ex) {
+//            Logger.getLogger(ABMUsuario.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return output;
+//
+//    }
 
     @Override
     public void eliminar(Long idUsuario) {
@@ -61,8 +85,20 @@ public class ABMUsuario implements ABMUsuarioLocal {
     }
 
     @Override
-    public Usuario modificar(Usuario u) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public String modificar(Long idUsuario, String nombre, String apellido, Date fNacimiento, String rol) {
+
+        Usuario user = this.obtener(idUsuario);
+        user.setNombre(nombre);
+        user.setApellido(apellido);
+        user.setFechaNacimiento(fNacimiento);
+        user.getCredencial().setRol(rol);
+        try {
+        em.merge(user);
+        return "Success";
+        } catch (Exception e) {
+            return "Fail";
+        }
+
     }
 
 //    public Usuario obtener(String login) {
@@ -97,6 +133,16 @@ public class ABMUsuario implements ABMUsuarioLocal {
          String jpl = "SELECT u FROM Usuario u";
         Query q = em.createQuery(jpl);
         return (List<Usuario>)q.getResultList();
+    }
+
+    @Override
+    public Usuario obtener(String login) {
+        String jpl = "Select u FROM Usuario u,Credenciales c WHERE u.CREDENCIAL_ID=c.ID AND c.login=:usuario";
+        Query q=em.createNamedQuery(jpl);
+        q.setParameter("usuario", login);
+        List<Usuario> userlist = (List<Usuario>) q.getResultList();
+        return userlist.get(0);
+
     }
     
   
