d11399efd83900d8927935ba0dd99811c671a0ec
pablodmv <pablodmv@c292032a-868c-c56d-4c54-da6379c2254a>
2010-12-08 01:09:55 +0000

    beta 1 - Implementaciones completas.

--- trunk/FrameworkDC/src/frameworkp/FrontController.java
+++ trunk/FrameworkDC/src/frameworkp/FrontController.java
@@ -43,7 +43,7 @@ public class FrontController extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        
+        request.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
